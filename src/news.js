00e8782e20a0edf6ed17a13ecf4ee6a1b9aa336a
hariswb <hariswb@gmail.com>
2020-07-21 15:13:36 +0700

    add proxy CORS and change newsapi codes form npm package to fetch url

--- src/news.js
+++ src/news.js
@@ -5,37 +5,51 @@ import "./dashboard.css"
 
 const News = (props)=>{
 
-	const today = new Date()
-	const twoWeeksBefore = new Date(today.getTime()-1.21e+9*2)
+	function dateISO8601(dateobject){
+		return [
+			dateobject.getFullYear(), 
+			dateobject.getMonth()+1, 
+			dateobject.getDate()
+			]
+			.map(x=>x.toString()).join('-')
+	}
+
+	const today = dateISO8601(new Date())
+	const twoWeeksAgo = dateISO8601(new Date(new Date().getTime()-1.21e+9*1))
 
 	const [news,setNews] = useState([])
 
-	const query = props.countryData.language === "en"?
-	{
-		q: "+coronavirus"+ " AND " +"+"+props.countryData.name,
-		from: twoWeeksBefore,
-		to: today,
-		language: "en",
-		pageSize: 20
-	}:
-	{	
-		q: "coronavirus" ,
-		from: twoWeeksBefore,
-		to: today,
-		country: props.countryData.iso.toLowerCase()
-	}
-	
 	async function fetchNews(){
-		
-		const NewsAPI = require('newsapi')
-		const newsapi = new NewsAPI('4c389de1e74e4a8db25d6c977926348f')
-		const response = await props.countryData.language==="en"?await newsapi.v2.everything(query): await newsapi.v2.topHeadlines(query)
-		// const response = await newsapi.v2.everything(query)
-		setNews(await response.articles)
+		const proxyurl = "https://cors-anywhere.herokuapp.com/";
+		const everythingUrl = 
+					proxyurl +
+					'http://newsapi.org/v2/everything?' +
+		  		'q=+coronavirus'+ " AND "+"+"+props.countryData.name+"&"+
+          'language=en&' +
+          'pageSize=20&'+
+          'from='+today+"&"+
+          'to='+twoWeeksAgo+"&"+
+          'apiKey=4c389de1e74e4a8db25d6c977926348f';
+
+    const topheadlinesUrl =
+					proxyurl +
+    			'http://newsapi.org/v2/top-headlines?' +
+		  		'country='+props.countryData.iso.toLowerCase()+'&'+
+		  		'q=+coronavirus&'+ 
+		  		'from='+today+"&"+
+          'to='+twoWeeksAgo+"&"+
+          'apiKey=4c389de1e74e4a8db25d6c977926348f';
+
+          console.log(today,twoWeeksAgo)
+
+		const req = new Request(props.countryData.language==="en"?everythingUrl:topheadlinesUrl);	
+		let response = await fetch(req)
+		let data = await response.json()
+		setNews(await data.articles)
 	}
 
 	useEffect(()=>{
-		fetchNews(query);
+		fetchNews();
 	},[props.countryData]);
 	
 	const headlines = 
@@ -60,10 +74,10 @@ const News = (props)=>{
 
 	const empty = 
 			<div className={"empty"}>
-				<h4>We're sorry, nothing to thow here</h4>
+				<h4>We're sorry, news in that country's language is not available. Please use the english version. </h4>
 			</div>
 		
-
+	
 	return(
 		<div className={"newslist"}>
 			{headlines.length>0?headlines:empty}
